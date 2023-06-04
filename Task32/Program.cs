781ea5c08bab9bbc8bdb3e636c3ff687c2a30423
Anton <B_atonyo_S@bk.ru>
2023-06-04 09:05:24 +0300

    Task32 completed

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]*/
+int[] GetRandomArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++) result[i] = new Random().Next(min, max + 1);
+    return result;
+}
+int[] Change(int[] array){
+    int [] result = new int [array.Length];
+    for (int i=0; i<array.Length; i++) result[i] =array[i]*-1;  
+    return result;
+    } 
+int Enter(string n){
+    System.Console.WriteLine(n);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+Console.Clear();
+int size = Enter("Введите количество элементов массива");
+int min = Enter("Введите минимальное значение каждого из элементов массива");
+int max = Enter("Введите максимальное значение каждого из элементов массива");
+int [] array = GetRandomArray(size,min,max);
+Console.Write($" {string.Join(" ", array)} => [{string.Join(" ", Change(array))} ]");
+System.Console.WriteLine();
+
